c7aa93ad69c7b20f4c58160b1e3a46c60c9b7d7f
Davias Leaptoken <115111841+LeapToken@users.noreply.github.com>
2023-08-03 15:55:22 -0600

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,48 +1,49 @@
 window.addEventListener('load', function() {
   var input = document.getElementById("input");
   input.addEventListener('keyup', function onEvent(e) {
-      if (e.keyCode === 13) {
-          showLoadingOverlay(); // Show the loading screen
-          go();
-      }
+    if (e.keyCode === 13) {
+      showLoadingOverlay(); // Show the loading screen
+      go();
+    }
   });
-});
 
-function showLoadingOverlay() {
-  var loadingOverlay = document.getElementById("loadingOverlay");
-  loadingOverlay.style.display = "flex";
-}
-
-function hideLoadingOverlay() {
-  var loadingOverlay = document.getElementById("loadingOverlay");
-  loadingOverlay.style.display = "none";
-}
-
-// Remove the "show" class after a short delay to allow the fade-out animation to complete
-  setTimeout(function() {
-    loadingOverlay.classList.remove('show');
-  }, 300); // The same duration as the CSS transition (0.3s) in milliseconds
-}
-
-// Function to handle page navigation (go back)
-function handlePageNavigation(event) {
-  if (event.persisted) {
-    hideLoadingOverlay();
+  // Function to show the loading screen
+  function showLoadingOverlay() {
+    var loadingOverlay = document.getElementById("loadingOverlay");
+    loadingOverlay.style.display = "flex";
   }
-}
 
+  // Function to hide the loading screen
+  function hideLoadingOverlay() {
+    var loadingOverlay = document.getElementById("loadingOverlay");
+    loadingOverlay.style.display = "none";
+
+    // Remove the "show" class after a short delay to allow the fade-out animation to complete
+    setTimeout(function() {
+      loadingOverlay.classList.remove('show');
+    }, 300); // The same duration as the CSS transition (0.3s) in milliseconds
+  }
+
+  // Function to handle page navigation (go back)
+  function handlePageNavigation(event) {
+    if (event.persisted) {
+      hideLoadingOverlay();
+    }
+  }
+
+  // Attach event listener to handle page navigation (go back)
   window.addEventListener('pageshow', handlePageNavigation);
-});
 
-function go() {
-  var url = document.getElementById("input").value;
-  if (url.trim() !== "") {
-    window.onbeforeunload = showLoadingOverlay; // Show loading screen before redirecting
-    window.location.href = "/service/gateway?url=" + url;
+  function go() {
+    var url = document.getElementById("input").value;
+    if (url.trim() !== "") {
+      window.onbeforeunload = showLoadingOverlay; // Show loading screen before redirecting
+      window.location.href = "/service/gateway?url=" + url;
+    }
   }
-}
 
-// Hide the loading screen when the new page has finished loading
-window.addEventListener('load', function() {
-  hideLoadingOverlay();
+  // Hide the loading screen when the new page has finished loading
+  window.addEventListener('load', function() {
+    hideLoadingOverlay();
+  });
 });
