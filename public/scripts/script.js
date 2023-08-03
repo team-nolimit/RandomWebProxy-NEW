a6bff8c7c093197917a75c0b30320568a5b1269f
Davias Leaptoken <115111841+LeapToken@users.noreply.github.com>
2023-08-03 15:32:31 -0600

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,14 +2,31 @@ window.addEventListener('load', function() {
   var input = document.getElementById("input");
   input.addEventListener('keyup', function onEvent(e) {
       if (e.keyCode === 13) {
+          showLoadingOverlay(); // Show the loading screen
           go();
       }
   });
 });
 
+function showLoadingOverlay() {
+  var loadingOverlay = document.getElementById("loadingOverlay");
+  loadingOverlay.style.display = "flex";
+}
+
+function hideLoadingOverlay() {
+  var loadingOverlay = document.getElementById("loadingOverlay");
+  loadingOverlay.style.display = "none";
+}
+
 function go() {
   var url = document.getElementById("input").value;
   if (url.trim() !== "") {
+    window.onbeforeunload = showLoadingOverlay; // Show loading screen before redirecting
     window.location.href = "/service/gateway?url=" + url;
   }
 }
+
+// Hide the loading screen when the new page has finished loading
+window.addEventListener('load', function() {
+  hideLoadingOverlay();
+});
