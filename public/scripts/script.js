2e9940003df507b3f3f25068b03d05e3ec8f41cf
Davias Leaptoken <115111841+LeapToken@users.noreply.github.com>
2023-08-03 15:40:56 -0600

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,21 +1,26 @@
 window.addEventListener('load', function() {
   var input = document.getElementById("input");
   input.addEventListener('keyup', function onEvent(e) {
-      if (e.keyCode === 13) {
-          showLoadingOverlay(); // Show the loading screen
-          go();
-      }
+    if (e.keyCode === 13) {
+      showLoadingOverlay(); // Show the loading screen
+      go();
+    }
   });
 });
 
 function showLoadingOverlay() {
   var loadingOverlay = document.getElementById("loadingOverlay");
-  loadingOverlay.style.display = "flex";
+  loadingOverlay.classList.add('show'); // Add "show" class to trigger fade-in
 }
 
 function hideLoadingOverlay() {
   var loadingOverlay = document.getElementById("loadingOverlay");
-  loadingOverlay.style.display = "none";
+  loadingOverlay.classList.add('hide'); // Add "hide" class to trigger fade-out
+
+  // Remove the "show" class after a short delay to allow the fade-out animation to complete
+  setTimeout(function() {
+    loadingOverlay.classList.remove('show');
+  }, 300); // The same duration as the CSS transition (0.3s) in milliseconds
 }
 
 function go() {
