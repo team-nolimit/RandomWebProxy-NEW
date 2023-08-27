6bf343cdf76ecb205e6667bf5443f045863720b9
Davias Leaptoken <115111841+LeapToken@users.noreply.github.com>
2023-08-26 18:21:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const port = "8080";
-const Corrosion = require('./lib/server');
-const express = require('express');
-const app = express();
+const port = "8080"
+const Corrosion = require('./lib/server')
+const express = require('express')
+const app = express()
 
 const proxy = new Corrosion({
     prefix: "/service/",
@@ -11,20 +11,20 @@ const proxy = new Corrosion({
     requestMiddleware: [
         Corrosion.middleware.blacklist([
             "accounts.google.com",
-        ], "/blocked.html"), // Redirect to the blocked.html page
+        ], "Page blocked for security reasons!"),
     ]
+})
+
+app.use('/', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/public/index.html")
 });
 
-app.use('/', function (req, res, next) {
-    if (req.url === "/blocked.html") {
-        // Serve blocked.html directly
-        res.sendFile(__dirname + "/public/blocked.html");
-    } else {
-        // Proxy all other requests
-        proxy.request(req, res);
-    }
+app.use('/', function (req, res) {
+  proxy.request(req,res)
 });
 
 app.listen(process.env.PORT || port, () => {
-    console.log(`RandomWebProxy is running at localhost:${port}`);
-});
+  console.log(`RandomWebProxy is running at localhost:${port}`)
+})
