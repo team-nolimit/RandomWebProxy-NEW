5fd7a4fd9f4ecc18f14da410f4b1d024e641da61
Davias Leaptoken <115111841+LeapToken@users.noreply.github.com>
2023-08-26 18:15:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,19 +11,18 @@ const proxy = new Corrosion({
     requestMiddleware: [
         Corrosion.middleware.blacklist([
             "accounts.google.com",
-        ], "/pages/blocked.html"), // Redirect to the blocked.html page
+        ], "/blocked.html"), // Redirect to the blocked.html page
     ]
 });
 
-app.use('/', express.static(__dirname + '/public'));
-
-// Route for serving the blocked.html page
-app.get('/blocked.html', (req, res) => {
-    res.sendFile(__dirname + "/public/blocked.html");
-});
-
-app.use('/', function (req, res) {
-    proxy.request(req, res);
+app.use('/', function (req, res, next) {
+    if (req.url === "/blocked.html") {
+        // Serve blocked.html directly
+        res.sendFile(__dirname + "/public/blocked.html");
+    } else {
+        // Proxy all other requests
+        proxy.request(req, res);
+    }
 });
 
 app.listen(process.env.PORT || port, () => {
