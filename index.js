f52dc45d03f31e006e1175167f2fd8d82a696c9a
Davias Leaptoken <115111841+LeapToken@users.noreply.github.com>
2023-09-21 11:20:28 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-const port = "8080"
-const Corrosion = require('./lib/server')
-const express = require('express')
-const app = express()
-
-const proxy = new Corrosion({
-    prefix: "/service/",
-    codec: "xor",
-    title: "RandomWebProxy",
-    forceHttps: true,
-    requestMiddleware: [
-        Corrosion.middleware.blacklist([
-            "accounts.google.com",
-        ], "Page blocked for security reasons!"),
-    ]
-})
-
-app.use('/', express.static(__dirname + '/public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/public/index.html")
-});
-
-app.use('/', function (req, res) {
-  proxy.request(req,res)
-});
-
-app.listen(process.env.PORT || port, () => {
-  console.log(`RandomWebProxy is running at localhost:${port}`)
-})
+var req=require,
+  server=req("http").Server(),
+  proxy=new(req("./"))({encode: 'plain',title:"Rhodium",server:server, wss: true, corrosion: [false, {}], favicon: 'https://discord.com/'});proxy.init();server.on("request",(e,r)=>(e.url.startsWith(proxy.prefix)?proxy.request(e,r):r.end(req("fs").readFileSync("./index.html")))).listen(process.env.PORT||8080);
