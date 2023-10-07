730d0d20d442135637d19727838ea306678bcbaa
Davias Leaptoken <115111841+LeapToken@users.noreply.github.com>
2023-10-07 12:12:49 -0600

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -7,27 +7,25 @@ function handleKeyPress(e) {
   }
 }
 
-window.addEventListener('load', function() {
+// Add an event listener to the input field for Enter key press
+window.addEventListener('load', function () {
   var input = document.getElementById("input");
-  input.addEventListener('keyup', function onEvent(e) {
-      if (e.keyCode === 13) {
-          go();
-      }
-  });
+  input.addEventListener('keyup', handleKeyPress);
 });
 
-function go() {
-  var url = document.getElementById("input").value;
-  if (url.trim() !== "") {
-    window.location.href = "/service/gateway?url=" + url;
-  }
-}
-
-  // Function to show the loading screen with a random message
+// Function to show the loading screen with a random message
 function showLoadingOverlay() {
   var loadingOverlay = document.getElementById("loadingOverlay");
   loadingOverlay.style.display = "flex";
   updateLoadingMessage(); // Update the loading message with a random one
+}
+
+function go() {
+  showLoadingOverlay();
+  var url = document.getElementById("input").value;
+  if (url.trim() !== "") {
+    window.location.href = "/service/gateway?url=" + url;
+  }
 }
 
 // Function to hide the loading screen
@@ -35,21 +33,21 @@ function hideLoadingOverlay() {
   var loadingOverlay = document.getElementById("loadingOverlay");
   loadingOverlay.style.display = "none";
 
-    // Remove the "show" class after a short delay to allow the fade-out animation to complete
-    setTimeout(function() {
-      loadingOverlay.classList.remove('show');
-    }, 300); // The same duration as the CSS transition (0.3s) in milliseconds
-  }
+  // Remove the "show" class after a short delay to allow the fade-out animation to complete
+  setTimeout(function() {
+    loadingOverlay.classList.remove('show');
+  }, 300); // The same duration as the CSS transition (0.3s) in milliseconds
+}
 
-  // Function to handle page navigation (go back)
-  function handlePageNavigation(event) {
-    if (event.persisted) {
-      hideLoadingOverlay();
-    }
+// Function to handle page navigation (go back)
+function handlePageNavigation(event) {
+  if (event.persisted) {
+    hideLoadingOverlay();
   }
+}
 
-  // Attach event listener to handle page navigation (go back)
-  window.addEventListener('pageshow', handlePageNavigation);
+// Attach event listener to handle page navigation (go back)
+window.addEventListener('pageshow', handlePageNavigation);
 
   // Hide the loading screen when the new page has finished loading
   window.addEventListener('load', function() {
