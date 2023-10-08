f430aaaa7e4e9c89dd3c4d5f2540a24cae2ff965
Davias Leaptoken <115111841+LeapToken@users.noreply.github.com>
2023-10-08 09:07:46 -0600

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,53 +1,55 @@
+// Function to handle Enter key press in the search bar
+function handleKeyPress(e) {
+  if (e.keyCode === 13) {
+    // User pressed Enter in the search bar, so show the loading overlay and perform the search
+    showLoadingOverlay();
+    go(); // Call the existing go() function for the search
+  }
+}
+
 window.addEventListener('load', function() {
   var input = document.getElementById("input");
   input.addEventListener('keyup', function onEvent(e) {
-      if (e.keyCode === 13) {
-          go();
-      }
+    if (e.keyCode === 13) {
+      showLoadingOverlay(); // Show the loading screen
+      go();
+    }
   });
-});
 
-// Add an event listener to the input field for Enter key press
-window.addEventListener('load', function () {
-  var input = document.getElementById("input");
-  input.addEventListener('keyup', handleKeyPress);
-});
-
-// Function to show the loading screen with a random message
-function showLoadingOverlay() {
-  var loadingOverlay = document.getElementById("loadingOverlay");
-  loadingOverlay.style.display = "flex";
-  updateLoadingMessage(); // Update the loading message with a random one
-}
-
-function go() {
-  showLoadingOverlay();
-  var url = document.getElementById("input").value;
-  if (url.trim() !== "") {
-    window.location.href = "/service/gateway?url=" + url;
+  // Function to show the loading screen
+  function showLoadingOverlay() {
+    var loadingOverlay = document.getElementById("loadingOverlay");
+    loadingOverlay.style.display = "flex";
   }
-}
 
-// Function to hide the loading screen
-function hideLoadingOverlay() {
-  var loadingOverlay = document.getElementById("loadingOverlay");
-  loadingOverlay.style.display = "none";
+  // Function to hide the loading screen
+  function hideLoadingOverlay() {
+    var loadingOverlay = document.getElementById("loadingOverlay");
+    loadingOverlay.style.display = "none";
 
-  // Remove the "show" class after a short delay to allow the fade-out animation to complete
-  setTimeout(function() {
-    loadingOverlay.classList.remove('show');
-  }, 300); // The same duration as the CSS transition (0.3s) in milliseconds
-}
-
-// Function to handle page navigation (go back)
-function handlePageNavigation(event) {
-  if (event.persisted) {
-    hideLoadingOverlay();
+    // Remove the "show" class after a short delay to allow the fade-out animation to complete
+    setTimeout(function() {
+      loadingOverlay.classList.remove('show');
+    }, 300); // The same duration as the CSS transition (0.3s) in milliseconds
   }
-}
 
-// Attach event listener to handle page navigation (go back)
-window.addEventListener('pageshow', handlePageNavigation);
+  // Function to handle page navigation (go back)
+  function handlePageNavigation(event) {
+    if (event.persisted) {
+      hideLoadingOverlay();
+    }
+  }
+
+  // Attach event listener to handle page navigation (go back)
+  window.addEventListener('pageshow', handlePageNavigation);
+
+  function go() {
+    var url = document.getElementById("input").value;
+    if (url.trim() !== "") {
+      window.onbeforeunload = showLoadingOverlay; // Show loading screen before redirecting
+      window.location.href = "/service/gateway?url=" + url;
+    }
+  }
 
   // Hide the loading screen when the new page has finished loading
   window.addEventListener('load', function() {
